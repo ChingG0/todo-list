499615abac502c3697904bdc9e194d64890abb0e
ChingG0 <www.karta2428620@gmail.com>
2022-07-15 13:30:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
 
-app.get('/',(req,res) =>{
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true }) //connect to MongoDB
+
+const db  = mongoose.connection
+db.on('error',()=>{
+    console.log('mongodb error')
+})
+
+db.once('open', ()=>{
+    console.log('mongodb connected!')
+})
+
+app.get('/',(req,res)=>{
     res.send('HELLO WORLD')
 })
 
