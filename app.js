b169df6f10f868588ff06be53ee30f8d34a4780c
ChingG0 <www.karta2428620@gmail.com>
2022-07-18 17:16:55 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
-
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
+const methodOverride = require('method-override')
 
 const Todo = require("./models/todo")
 
@@ -27,6 +27,7 @@ app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }))
 app.set("view engine", "hbs")
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get("/", (req, res) => {
   Todo.find()
@@ -47,7 +48,7 @@ app.post("/todos", (req, res) => {
 
   return Todo.create({ name })
     .then(() => res.redirect("/"))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err)) 
 })
 
 app.get("/todos/:id", (req, res) => {
@@ -68,7 +69,7 @@ app.get("/todos/:id/edit", (req, res) => {
     .catch((err) => console.log(err))
 })
 
-app.post("/todos/:id/edit", (req, res) => {
+app.put("/todos/:id", (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   
@@ -83,7 +84,7 @@ app.post("/todos/:id/edit", (req, res) => {
     .catch((err) => console.log(err))
 })
 
-app.post("/todos/:id/delete", (req, res) => {
+app.delete("/todos/:id", (req, res) => {
   const id = req.params.id
 
   return Todo.findById(id)
